22ab8c3cb7f4e1d4a087a9385fd535167b44dc42
raathmd <raathmd@github.com>
2025-06-26 17:21:53 +0000

    create quotes context and schema

---
+++ lib/tragar_cms/quotes.ex
@@ -0,0 +1,84 @@
+defmodule TragarCms.Quotes do
+  @moduledoc """
+  The Quotes context.
+  """
+
+  import Ecto.Query, warn: false
+  alias TragarCms.Repo
+
+  alias TragarCms.Quotes.Quote
+
+  @doc """
+  Returns the list of quotes.
+
+  ## Examples
+
+      iex> list_quotes()
+      [%Quote{}, ...]
+
+  """
+  def list_quotes do
+    Repo.all(Quote)
+  end
+
+  @doc """
+  Gets a single quote.
+
+  ## Examples
+
+      iex> get_quote!(123)
+      %Quote{}
+
+      iex> get_quote!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_quote!(id), do: Repo.get!(Quote, id)
+
+  @doc """
+  Creates a quote.
+
+  ## Examples
+
+      iex> create_quote(%{field: value})
+      {:ok, %Quote{}}
+
+      iex> create_quote(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_quote(attrs \\ %{}) do
+    %Quote{}
+    |> Quote.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Deletes a quote.
+
+  ## Examples
+
+      iex> delete_quote(quote)
+      {:ok, %Quote{}}
+
+      iex> delete_quote(quote)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_quote(%Quote{} = quote) do
+    Repo.delete(quote)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking quote changes.
+
+  ## Examples
+
+      iex> change_quote(quote)
+      %Ecto.Changeset{}}
+
+  """
+  def change_quote(%Quote{} = quote, attrs \\ %{}) do
+    Quote.changeset(quote, attrs)
+  end
+end
